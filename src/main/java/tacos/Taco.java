4bafe93b5758d316059fb0a59ba00339e496f8b4
Michele delle Noci <michele.dellenoci@gmail.com>
2022-05-26 21:23:26 +0200

    Switch to use spring data jpa

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -3,14 +3,22 @@ package tacos;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
 
 @Data
+@Entity
 public class Taco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date createdAt = new Date();
@@ -21,5 +29,6 @@ public class Taco {
 
     @NotNull
     @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<IngredientRef> ingredients;
+    @ManyToMany
+    private List<Ingredient> ingredients;
 }
