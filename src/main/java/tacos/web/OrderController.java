cc617113f8d705b0c1b8d31c3d740038f16d2036
Michele delle Noci <michele.dellenoci@gmail.com>
2022-05-26 19:06:11 +0200

    Add order repository

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -12,12 +12,19 @@ import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.TacoOrder;
+import tacos.data.OrderRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+
+    private OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
     
     @GetMapping("/current")
     public String orderForm() {
@@ -30,6 +37,7 @@ public class OrderController {
         if (errors.hasErrors()) {
             return "orderForm";
         }
+        orderRepo.save(order);
 
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
