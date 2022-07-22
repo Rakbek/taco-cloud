84231d68486019e8234afea0eb648594f9d45a97
Michele delle Noci <michele.dellenoci@gmail.com>
2022-07-22 08:19:55 +0200

    Remove unused imports

--- src/main/java/tacos/messaging/JmsOrderMessagingService.java
+++ src/main/java/tacos/messaging/JmsOrderMessagingService.java
@@ -2,10 +2,8 @@ package tacos.messaging;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.Session;
 
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
 
 import tacos.TacoOrder;
